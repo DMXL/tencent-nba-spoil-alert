b0c0f672f8722e9bd3a3a27eca19941baeed57b1
Daniel <dm@dmon-studio.com>
2022-02-06 17:05:33 +0800

    Add run_at for game script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
 # On Windows WSL
-taskkill.exe /im chrome.exe /f && /c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe \
+
+# Kill all Chrome tasks
+taskkill.exe /im chrome.exe /f
+# Open page with unpacked extension loaded
+/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe \
 https://nba.stats.qq.com/schedule/ \
 --load-extension="E:\Projects\tencent-nba-spoil-alert"
+
+Exit 0
