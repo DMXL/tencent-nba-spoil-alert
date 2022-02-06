7c875c588fc33b698d978c679c219dee47f5a23f
Daniel <dm@dmon-studio.com>
2022-02-07 00:07:32 +0800

    Handle game data spoil alert

--- js/game.js
+++ js/game.js
@@ -4,20 +4,53 @@ const {addShowResultButton} = require('./utils');
 const header = $('#head-box > .inner');
 const datetime = header.find('.info > .inner > .vip-datetime-wrap');
 
-addShowResultButton(datetime, () => header.find('.info > .inner').addClass('show'), 'div');
+addShowResultButton({
+	container: datetime,
+	onClick: () => header.find('.info > .inner').addClass('show'),
+	tag: 'div',
+});
 
-const observer = new MutationObserver(() => {
+const headerObserver = new MutationObserver((mutations, observer) => {
 	const inner = $('#head-box > .inner > .info > .inner');
 	const wrap = header.children('.vip-datetime-wrap');
 
-	addShowResultButton(wrap, () => inner.addClass('show'), 'div');
+	if (addShowResultButton({
+		container: wrap,
+		onClick: () => inner.addClass('show'),
+		tag: 'div',
+	})) {
+		observer.disconnect();
+	}
 });
 
-const target = header[0];
-
-observer.observe(target, {
+headerObserver.observe(header[0], {
 	subtree: true,
 	attributes: true,
 	childList: true,
 	characterData: true,
 });
+
+let i = 1;
+
+const dataObserver = new MutationObserver((mutations, observer) => {
+	console.log(`called ${i} time${i > 1 ? 's' : ''}`);
+	i += 1;
+	const container = $('#data-box .left-data-tabs');
+
+	if (addShowResultButton({
+		container,
+		onClick: () => container.addClass('show'),
+		tag: 'div',
+		buttonText: '显示数据',
+	})) {
+		observer.disconnect();
+	}
+});
+
+dataObserver.observe($('#container')[0], {
+	subtree: true,
+	attributes: true,
+	childList: true,
+	characterData: true,
+});
+
